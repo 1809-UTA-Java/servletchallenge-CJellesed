d5fbc3dd19c49476f9c0bce6acf36173e8220fbb
Me <Me@10.182.76.76>
2018-10-12 10:37:56 -0600

    What I have so far.

--- servletchallenge/src/main/java/com/revature/servletchallenge/App.java
+++ servletchallenge/src/main/java/com/revature/servletchallenge/App.java
@@ -1,14 +1,26 @@
 package com.revature.servletchallenge;
 
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * Hello world!
  *
  */
-public class App 
+
+@WebServlet("/home")
+public class App extends HttpServlet 
 {
-    public static void main( String[] args )
-    {
-    	
-        System.out.println( "Hello World!" );
-    }
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+			throws ServletException, IOException {
+		RequestDispatcher rd = req.getRequestDispatcher("index.html");
+		rd.forward(req, resp);
+	}
 }
